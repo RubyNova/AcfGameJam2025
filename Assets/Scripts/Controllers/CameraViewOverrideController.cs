f4a3fbf6a416b213dd97cf6e24128de60a1c04e1
Ken Johnson <k.rjohnson94@outlook.com>
2025-04-18 17:44:25 -0400

    Add QOL updates to level 4

---
+++ Assets/Scripts/Controllers/CameraViewOverrideController.cs
@@ -0,0 +1,124 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Controllers 
+{
+    public class CameraViewOverrideController : MonoBehaviour
+    {
+        [Header("Dependencies")]
+        [SerializeField] private CinemachineCamera _cinemachineCameraToOverride;
+        [SerializeField] private CinemachineOrbitalFollow _orbitalFollowToOverride;
+
+        [Header("Configuration")]
+        [SerializeField] private float _orthographicSizeOverride;
+        [SerializeField] private bool _overrideTrackingPosition = false;
+        [SerializeField] private Vector3 _trackingPositionOverride = Vector3.zero;
+
+        private float _originalOrthographicSize = 0.0f;
+        private Vector3 _originalTrackingPosition = Vector3.zero;
+
+        private bool _switchingToOriginal = false;
+        private bool _switchingToOverride = false;
+        private float _lerpAmount = 0.0f;
+        private Vector3 _vectorLerp = Vector3.zero;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            if(_cinemachineCameraToOverride is not null)
+            {
+                _originalOrthographicSize = _cinemachineCameraToOverride.Lens.OrthographicSize;
+                _lerpAmount = (_orthographicSizeOverride - _originalOrthographicSize) * Time.fixedDeltaTime;
+            }
+            if(_orbitalFollowToOverride is not null)
+            {
+                _originalTrackingPosition = _orbitalFollowToOverride.TargetOffset;
+                _vectorLerp = (_trackingPositionOverride - _originalTrackingPosition) * Time.fixedDeltaTime;
+            }
+        }
+
+        void FixedUpdate()
+        {   
+            if(_switchingToOverride)
+            {
+                LerpToOverride();
+            }
+
+            if(_switchingToOriginal)
+            {
+                LerpFromOverride();
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == "Player" || collision.tag == "Familiar")
+            {
+                OverrideCamera();
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            if(collision.tag == "Player" || collision.tag == "Familiar")
+            {
+                ResetCamera();
+            }
+        }
+
+        void LerpToOverride()
+        {
+            if(_cinemachineCameraToOverride is not null)
+            {
+                _cinemachineCameraToOverride.Lens.OrthographicSize += _lerpAmount;
+                if(_cinemachineCameraToOverride.Lens.OrthographicSize > _orthographicSizeOverride)
+                {
+                    _cinemachineCameraToOverride.Lens.OrthographicSize = _orthographicSizeOverride;
+                    _switchingToOverride = false;
+                }   
+            }       
+            if(_orbitalFollowToOverride is not null)
+            {
+                _orbitalFollowToOverride.TargetOffset += _vectorLerp;
+                if(Vector2.Distance(_orbitalFollowToOverride.TargetOffset, _trackingPositionOverride) < 0.1)
+                {
+                    _orbitalFollowToOverride.TargetOffset = _trackingPositionOverride;
+                }
+            }
+        }
+
+        void LerpFromOverride()
+        {
+            if(_cinemachineCameraToOverride is not null)
+            {
+                _cinemachineCameraToOverride.Lens.OrthographicSize -= _lerpAmount;
+                if(_cinemachineCameraToOverride.Lens.OrthographicSize < _originalOrthographicSize)
+                {
+                    _cinemachineCameraToOverride.Lens.OrthographicSize = _originalOrthographicSize;
+                    _switchingToOverride = false;
+                }   
+            }       
+            if(_orbitalFollowToOverride is not null)
+            {
+                _orbitalFollowToOverride.TargetOffset -= _vectorLerp;
+                if(Vector2.Distance(_orbitalFollowToOverride.TargetOffset, _originalTrackingPosition) < 0.1)
+                {
+                    _orbitalFollowToOverride.TargetOffset = _originalTrackingPosition;
+                }
+            }
+        }
+
+        public void ResetCamera()
+        {
+            _switchingToOriginal = true;
+            _switchingToOverride = false;
+        }
+
+        public void OverrideCamera()
+        {
+            _switchingToOriginal = false;
+            _switchingToOverride = true;
+        }
+    }
+}
