20ed657214e2fb7758b1c8bb7857e682c61f9161
Matt Jones <matthewj866@gmail.com>
2025-04-19 14:26:04 +0100

    Add gameobject disabling support to ColliderFunctionTrigger

--- Assets/Scripts/Environment/ColliderFunctionTrigger.cs
+++ Assets/Scripts/Environment/ColliderFunctionTrigger.cs
@@ -5,13 +5,19 @@ namespace Environment
 {
     public class ColliderFunctionTrigger : MonoBehaviour
     {
-        [SerializeField] public UnityEvent EventsToFire = new();
+        [SerializeField] private bool _disableGameObjectAfterTrigger = false;
+        [SerializeField] private UnityEvent EventsToFire = new();
 
         void OnTriggerEnter2D(Collider2D collision)
         {
             if(collision.tag == "Player" || collision.tag == "LightFamiliar")
             {
                 EventsToFire.Invoke();
+
+                if (_disableGameObjectAfterTrigger)
+                {
+                    gameObject.SetActive(false);
+                }
             }
         }
     }
