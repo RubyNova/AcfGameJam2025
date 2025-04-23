cf0fcaff364b5b5d6f1454c417e345495f04c9cc
Ken Johnson <k.rjohnson94@outlook.com>
2025-04-23 16:39:41 -0400

    Fix issue where movable beams disable and leave their effect on

--- Assets/Scripts/Environment/MoveableObjectModifier.cs
+++ Assets/Scripts/Environment/MoveableObjectModifier.cs
@@ -5,14 +5,29 @@ namespace Environment
 {
     public class MoveableObjectModifier : LightBeamModifier
     {
+        private GenericBeamForceReactor _cachedTarget = null;
+        private LightBeamController _cachedLbc = null;
+
         public override void ApplyBeamEffectToObject(LightBeamController sender, int beamPriority, GenericBeamForceReactor target, Vector2 senderBeamDirection)
         {
+            _cachedTarget = target;
+            _cachedLbc = sender;
             target.RegisterIncomingForce(sender, beamPriority, senderBeamDirection, BeamForce);
         }
 
         public override void ClearBeamEffectOnObject(LightBeamController sender, int beamPriority, GenericBeamForceReactor target)
         {
+            _cachedTarget = null;
+            _cachedLbc = null;
             target.UnregisterIncomingForce(sender, beamPriority);
+        }
+
+        void OnDisable()
+        {
+            if(_cachedTarget != null && _cachedLbc != null)
+            {
+                _cachedTarget.UnregisterIncomingForce(_cachedLbc, _cachedLbc.BeamPriority);
+            }
         }
     }
 }
